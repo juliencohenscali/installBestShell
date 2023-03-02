98fd3358422b48d61d946dcbd1b171a5e14f5a1f
juliencohenscali <julien.cohen-scali@epita.fr>
2023-03-02 17:57:15 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+cd /home/$USER/.oh-my-zsh/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions
+
+cd /home/$USER/.oh-my-zsh/themes
+git clone https://github.com/romkatv/powerlevel10k
+
+curl -o ~/.zshrc https://raw.githubusercontent.com/juliencohenscali/installBestShell/.zshrc
